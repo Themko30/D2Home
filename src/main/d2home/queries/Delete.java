eb76d169189f64cdd4cd8369565ff1fce5eda1e5
Alfonso D'Albenzio <alfonsodalbenzio5@gmail.com>
2022-01-23 16:14:28 +0100

    terminate Query Base

--- src/main/d2home/queries/Delete.java
+++ src/main/d2home/queries/Delete.java
@@ -1,20 +1,67 @@
 package main.d2home.queries;
 
-import javax.swing.*;
+import java.awt.event.ActionListener;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTable;
+import javax.swing.JTextField;
+import net.proteanit.sql.DbUtils;
 
 public class Delete extends JFrame {
 
-    private JPanel mainPanel;
-    private Connection con;
+  private JPanel mainPanel;
+  private JTable table1;
+  private JTextField utenteTextField;
+  private JTextField emailTextField1;
+  private JButton eseguiButton;
+  private Connection con;
 
-    public Delete(Connection con) {
-        setContentPane(mainPanel);
-        setTitle("Delete");
-        setVisible(true);
-        setBounds(300, 30, 900, 750);
+  public Delete(Connection con) {
+    setContentPane(mainPanel);
+    setTitle("Delete");
+    setVisible(true);
+    setBounds(300, 30, 900, 750);
 
-        this.con = con;
+    this.con = con;
+    eseguiButton.addActionListener(eseguiListener);
+    try {
+      PreparedStatement ps = con.prepareStatement("SELECT * FROM utente");
+      ResultSet rs = ps.executeQuery();
+      table1.setModel(DbUtils.resultSetToTableModel(rs));
+
+    } catch (Exception throwables) {
+      JOptionPane.showMessageDialog(
+          this, throwables.getMessage(), "Errore", JOptionPane.ERROR_MESSAGE);
+      throwables.printStackTrace();
     }
+  }
 
+  ActionListener eseguiListener =
+      e -> {
+        String email = emailTextField1.getText().trim();
+
+        try {
+          PreparedStatement ps = con.prepareStatement("DELETE FROM utente WHERE email=?");
+          ps.setString(1, email);
+
+          if (ps.executeUpdate() != 1) {
+            throw new Exception("Cancellazione fallita, controlla email e riprova.");
+          }
+          JOptionPane.showMessageDialog(
+              this, "Cancellazione riuscita!", "Successo", JOptionPane.INFORMATION_MESSAGE);
+          ps = con.prepareStatement("SELECT * FROM utente");
+          ResultSet rs = ps.executeQuery();
+          table1.setModel(DbUtils.resultSetToTableModel(rs));
+
+        } catch (Exception throwables) {
+          JOptionPane.showMessageDialog(
+              this, throwables.getMessage(), "Errore", JOptionPane.ERROR_MESSAGE);
+          throwables.printStackTrace();
+        }
+      };
 }
--- src/main/d2home/queries/Insert.java
+++ src/main/d2home/queries/Insert.java
@@ -2,12 +2,17 @@ package main.d2home.queries;
 
 import java.awt.event.ActionListener;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JPasswordField;
 import javax.swing.JTable;
 import javax.swing.JTextField;
+import main.d2home.MainWindow;
+import net.proteanit.sql.DbUtils;
 
 public class Insert extends JFrame {
 
@@ -40,18 +45,66 @@ public class Insert extends JFrame {
         String nome = nomeTextField2.getText().trim();
         String cognome = cognomeTextField3.getText().trim();
         String password = new String(passwordField1.getPassword());
+        String passwordhash = MainWindow.getHashFromPassword(password);
         String telefono = telefonoTextField5.getText().trim();
         String indirizzo = indirizzoTextField6.getText().trim();
         String admin = adminTextField7.getText().trim();
 
-        int a;
+        int a = 0;
         try {
+          if (email.equals(""))
+            throw new Exception("Inserimento fallito, controlla email e riprova.");
+          if (nome.equals(""))
+            throw new Exception("Inserimento fallito, controlla nome e riprova.");
+          if (cognome.equals(""))
+            throw new Exception("Inserimento fallito, controlla cognome e riprova.");
+          if (password.equals(""))
+            throw new Exception("Inserimento fallito, controlla password e riprova.");
+          if (telefono.equals(""))
+            throw new Exception("Inserimento fallito, controlla telefono e riprova.");
+          if (indirizzo.equals(""))
+            throw new Exception("Inserimento fallito, controlla indirizzo e riprova.");
           a = Integer.parseInt(admin);
           if (a != 0 && a != 1) {
-            throw new IllegalArgumentException();
+            throw new Exception("Inserimento fallito, controlla admin e riprova.");
           }
-        } catch (IllegalArgumentException exception) {
-          exception.printStackTrace();
+        } catch (NumberFormatException ne) {
+          JOptionPane.showMessageDialog(
+              this, "Errore in admin, solo numeri!", "Errore", JOptionPane.ERROR_MESSAGE);
+          ne.printStackTrace();
+          return;
+        } catch (Exception throwables) {
+          JOptionPane.showMessageDialog(
+              this, throwables.getMessage(), "Errore", JOptionPane.ERROR_MESSAGE);
+          throwables.printStackTrace();
+          return;
+        }
+
+        try {
+          PreparedStatement ps =
+              con.prepareStatement(
+                  "INSERT INTO utente (email, nome, cognome, indirizzo, telefono, password, admin) VALUES (?,?,?,?,?,?,?)");
+          ps.setString(1, email);
+          ps.setString(2, nome);
+          ps.setString(3, cognome);
+          ps.setString(4, indirizzo);
+          ps.setString(5, telefono);
+          ps.setString(6, passwordhash);
+          ps.setInt(7, a);
+
+          if (ps.executeUpdate() != 1) {
+            throw new Exception("Inserimento fallito, controlla i campi e riprova.");
+          }
+          JOptionPane.showMessageDialog(
+              this, "Inserimento riuscito!", "Successo", JOptionPane.INFORMATION_MESSAGE);
+          ps = con.prepareStatement("SELECT * FROM utente");
+          ResultSet rs = ps.executeQuery();
+          table1.setModel(DbUtils.resultSetToTableModel(rs));
+
+        } catch (Exception throwables) {
+          JOptionPane.showMessageDialog(
+              this, throwables.getMessage(), "Errore", JOptionPane.ERROR_MESSAGE);
+          throwables.printStackTrace();
         }
       };
 }
